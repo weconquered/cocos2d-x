f8e6f820db90988284c625a6151852d43e867805
James Chen <jianhua.chen@cocos2d-x.org>
2013-04-26 10:33:12 +0800

    Updating MainLayer.h/.cpp.

--- projects/helloccb/Classes/MainLayer.cpp
+++ projects/helloccb/Classes/MainLayer.cpp
@@ -7,3 +7,63 @@
 //
 
 #include "MainLayer.h"
+
+USING_NS_CC;
+USING_NS_CC_EXT;
+
+bool MainLayer::onAssignCCBMemberVariable(CCObject * pTarget, const char * pMemberVariableName, CCNode * pNode) {
+    CCB_MEMBERVARIABLEASSIGNER_GLUE(this, "mTopleftBtn", CCMenuItemImage*, this->mTopleftBtn);
+//    CCB_MEMBERVARIABLEASSIGNER_GLUE(this, "mTestTitleLabelTTF", CCLabelTTF *, this->mTestTitleLabelTTF);
+    
+    return false;
+}
+
+bool MainLayer::onAssignCCBCustomProperty(CCObject* pTarget, const char* pMemberVariableName, cocos2d::extension::CCBValue* pCCBValue)
+{
+    bool bRet = false;
+    if (pTarget == this)
+    {
+        if (0 == strcmp(pMemberVariableName, "mCustomPropertyInt"))
+        {
+            CCLog("mCustomPropertyInt = %d", pCCBValue->getIntValue());
+            bRet = true;
+        }
+//        else if ( 0 == strcmp(pMemberVariableName, "mCustomPropertyFloat"))
+//        {
+//            this->mCustomPropertyFloat = pCCBValue->getFloatValue();
+//            CCLog("mCustomPropertyFloat = %f", mCustomPropertyFloat);
+//            bRet = true;
+//        }
+//        else if ( 0  == strcmp(pMemberVariableName, "mCustomPropertyBoolean"))
+//        {
+//            this->mCustomPropertyBoolean = pCCBValue->getBoolValue();
+//            CCLog("mCustomPropertyBoolean = %d", mCustomPropertyBoolean);
+//            bRet = true;
+//        }
+//        else if ( 0  == strcmp(pMemberVariableName, "mCustomPropertyString"))
+//        {
+//            this->mCustomPropertyString = pCCBValue->getStringValue();
+//            CCLog("mCustomPropertyString = %s", mCustomPropertyString.c_str());
+//            bRet = true;
+//        }
+        
+    }
+    
+    return bRet;
+}
+
+SEL_MenuHandler MainLayer::onResolveCCBCCMenuItemSelector(CCObject * pTarget, const char * pSelectorName) {
+    CCB_SELECTORRESOLVER_CCMENUITEM_GLUE(this, "onPressButtonTopLeft", MainLayer::onTopLeftMenuClicked);
+    
+    return NULL;
+}
+
+SEL_CCControlHandler MainLayer::onResolveCCBCCControlSelector(CCObject * pTarget, const char * pSelectorName) {
+    return NULL;
+}
+
+void MainLayer::onTopLeftMenuClicked(cocos2d::CCObject * pSender)
+{
+    CCLog("onTopLeftMenuClicked...");
+    mTopleftBtn->setScale(2.0f);
+}
--- projects/helloccb/Classes/MainLayer.h
+++ projects/helloccb/Classes/MainLayer.h
@@ -14,9 +14,20 @@
 
 class MainLayer
 : public cocos2d::CCLayer
+, public cocos2d::extension::CCBMemberVariableAssigner
+, public cocos2d::extension::CCBSelectorResolver
 {
 public:
     CCB_STATIC_NEW_AUTORELEASE_OBJECT_WITH_INIT_METHOD(MainLayer, create);
+    virtual bool onAssignCCBMemberVariable(cocos2d::CCObject * pTarget, const char * pMemberVariableName, cocos2d::CCNode * pNode);
+    virtual bool onAssignCCBCustomProperty(CCObject* pTarget, const char* pMemberVariableName, cocos2d::extension::CCBValue* pCCBValue);
+    
+    virtual cocos2d::SEL_MenuHandler onResolveCCBCCMenuItemSelector(cocos2d::CCObject * pTarget, const char * pSelectorName);
+    virtual cocos2d::extension::SEL_CCControlHandler onResolveCCBCCControlSelector(cocos2d::CCObject * pTarget, const char * pSelectorName);
+    
+    void onTopLeftMenuClicked(cocos2d::CCObject * pSender);
+private:
+    cocos2d::CCMenuItemImage* mTopleftBtn;
 };
 
 
